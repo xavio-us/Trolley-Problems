548f2a073db037b584e9d11d6fd8b59d27be6b34
joseph-phb <256616040+joseph-phb@users.noreply.github.com>
2026-06-05 15:43:02 -0400

    Movement changes: added customizable amount of rails, changed movement to be on keypressed rather than keydown (it would spam up/down), changed rails positions to be dependent on number of rails (and moved up a little to always be seen/have player and enemies on rails, changed players and enemies to have a rail attribute

--- Trolly Game/main.lua
+++ Trolly Game/main.lua
@@ -3,6 +3,7 @@ enemy = {}
 enemyActive = false
 spawnTimer = 0
 spawnDelay = 0
+numRails = 6
 death = {}
 function love.conf(t)
     t.window.width = 720
@@ -12,8 +13,9 @@ end
 function love.load()
 	    -- This is the coordinates where the player character will be rendered.
 	player.x = 100   -- This sets the player at the middle of the screen based on the width of the game window. 
-	player.y = 240 or 480  -- This sets the player at the middle of the screen based on the height of the game window. 
-    enemy.x = -100
+	player.y = love.graphics.getHeight()/numRails  -- This sets the player at the middle of the screen based on the height of the game window. 
+    player.rail = 1
+	enemy.x = -100
 	enemy.y = 0
         -- This calls the file named "purple.png" and puts it in the variable called player.img.
 	player.img = love.graphics.newImage('purple.png')
@@ -29,26 +31,18 @@ function love.load()
 	death.img = love.graphics.newImage('death.jpg')
 end
 
-function love.update(dt)
-	if gamestate == true then
-		if love.keyboard.isDown('d') then
-			-- This makes sure that the character doesn't go pass the game window's right edge.
-			if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
-				player.x = player.x + (player.speed * dt)
-			end
-		elseif love.keyboard.isDown('a') then
-			-- This makes sure that the character doesn't go pass the game window's left edge.
-			if player.x > 0 then 
-				player.x = player.x - (player.speed * dt)
-			end
+function love.keypressed(keyid, key, isrepeat)
+	if gamestate == true then -- could be changed to represent more than 2 gamestates
+		if key == 'w' then
+			player.rail = ((player.rail + numRails - 2) % numRails) + 1 -- sub 1 and mod rails (offset due to 1 indexing)
+		else if key == 's' then
+			player.rail = (player.rail % numRails) + 1 -- add 1 mod rails
 		end
-		if love.keyboard.isDown('w')then
-			player.y = 240
-		end
-		if love.keyboard.isDown('s')then
-			player.y = 480
-		end
-		
+	end
+end
+
+function love.update(dt)		
+		player.y = player.rail * love.graphics.getHeight()/numRails -- move player to rail's location
 		
 		-- spawn delay system
 		if enemyActive == false then
@@ -58,8 +52,9 @@ function love.update(dt)
 				spawnTimer = 0
 				spawnDelay = math.random(1, 3)
 
+				enemy.rail = math.random(1,numRails)
 				enemy.x = love.graphics.getWidth()
-				enemy.y = (math.random(1,2) == 1) and 240 or 480
+				enemy.y = enemy.rail * love.graphics.getHeight()/numRails -- move enemy to rail's location
 
 				enemyActive = true
 			end
@@ -90,18 +85,19 @@ function checkCollision(a, b)
            a.y + a.img:getHeight() > b.y
 end
 function love.draw()
-	love.graphics.setColor(1, 1, 1)        -- This sets the platform color to white.
-	love.graphics.rectangle("fill", 0 , 240 , love.graphics.getWidth(), 5)
-	love.graphics.setColor(700, 700, 700)
-	love.graphics.rectangle("fill", 0 , 480 , love.graphics.getWidth(), 5)
-        -- The platform will now be drawn as a white rectangle while taking in the variables we declared above.
+	if gamestate == false then
+		love.graphics.print("Death", love.graphics.getWidth() / 2, love.graphics.getHeight()/2)
+		love.graphics.draw(death.img,0, 100, 0, 1.12)
+		return 
+	end
+	-- draw rails
+	love.graphics.setColor(1, 1, 1)        -- set rail color to white
+	for i = 1, numRails, 1 do
+		love.graphics.rectangle("fill", 0, (i*love.graphics.getHeight()/numRails)-5, love.graphics.getWidth(), 5)
+	end
+
 	love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
 	love.graphics.draw(enemy.img, enemy.x, enemy.y, 0, 1, 1, 0, 32)
 	love.graphics.print(tostring(enemyActive), 100,100)
-	if gamestate == false then
-		love.graphics.print("Death", love .graphics.getWidth() / 2, love.graphics.getHeight()/2)
-		love.graphics.draw(death.img,0, 100, 0, 1.12)
-
-	end
 end
 
